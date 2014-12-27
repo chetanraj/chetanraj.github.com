71a962d4660364cd35e2c3591139a556217df41c
chetanraj <chetan.narian@gmail.com>
2014-12-27 11:16:26 +0530

    some more changes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 requirejs.config({
     paths: {
-        "BigVideo": "bigvideo",
         "jquery": "jquery", //v2.1.3
         "jquery-ui": "jquery-ui", //v1.11.2
-        "videojs": "video",
-        "imagesloaded": "imagesloaded"
-    },
-    shim: {
-        "videojs": {exports: 'videojs'}
     }
 });
 
-define(["jquery", "jquery-ui", "BigVideo", "imagesloaded"], function($, ui, bigvideo, imagesLoaded ) {
+define(["jquery", "jquery-ui"], function($, ui ) {
     $(function() {
-        new imagesLoaded( document.querySelector('.container'), function( instance ) {
-            var BV = new $.BigVideo();
-            BV.init();
-            BV.show('/videos/Christmas_Tree_1.mov.mp4');
-        });
+        
     });
 });
