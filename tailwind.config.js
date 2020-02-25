cc96b7357509188b577ec4421d7218a725e91b5a
Chetan Raj <chetan.narian@gmail.com>
2020-02-25 11:24:46 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        colors: {
+            white: '#fff',
+            blue: '#0f4c81'
+        },
+        extend: {
+            spacing: {
+                80: '80vh',
+            }
+        }
+    }
+}
