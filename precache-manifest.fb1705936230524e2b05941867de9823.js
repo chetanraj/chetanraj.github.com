dbedfd62eeb722fd29b9707d4fb452b556074099
Chetan Raj <chetan.narian@gmail.com>
2020-04-08 16:18:32 +0530

    Changes: As per the design, Changed to React from Next

---
+++ precache-manifest.fb1705936230524e2b05941867de9823.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacdc673ed66df268a3b3aa4f1102cc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffc50eab814f01d6b2ef",
+    "url": "./static/css/main.6bae2617.chunk.css"
+  },
+  {
+    "revision": "ece413c9a09186371422",
+    "url": "./static/js/2.b28ff2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b28ff2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc50eab814f01d6b2ef",
+    "url": "./static/js/main.709e5455.chunk.js"
+  },
+  {
+    "revision": "3349ceb728dd7f2f052f",
+    "url": "./static/js/runtime-main.d201a9d3.js"
+  },
+  {
+    "revision": "d62b0dd174d1de73a0022067dde3b741",
+    "url": "./static/media/bg.d62b0dd1.jpg"
+  }
+]);
